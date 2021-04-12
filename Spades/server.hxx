72e198a3aa367c047b42d4ae66742af8c4bfb7bb
circumscriptor <circumscriptor@protonmail.com>
2021-04-12 18:14:42 +0200

    WIP: Add server constructor

--- Spades/server.hxx
+++ Spades/server.hxx
@@ -13,6 +13,44 @@ namespace spadesx {
 class server
 {
   public:
+    server() : server(32887)
+    {
+    }
+
+    server(std::uint16_t port) : server(port, 32)
+    {
+    }
+
+    server(std::uint16_t port, std::uint8_t connections) : server(port, connections, 0, 0)
+    {
+    }
+
+    server(std::uint16_t port, std::uint8_t connections, std::uint32_t bandwidth_in, std::uint32_t bandwidth_out)
+    {
+        std::cout << "initializing server\n";
+
+        if (enet_initialize() != 0) {
+            std::cerr << "failed to initalize ENet\n";
+            std::exit(EXIT_FAILURE);
+        }
+        atexit(enet_deinitialize);
+
+        ENetAddress address;
+        address.host = ENET_HOST_ANY;
+        address.port = port;
+
+        m_host = enet_host_create(&address, connections, 2, bandwidth_in, bandwidth_out);
+        if (m_host == nullptr) {
+            std::cerr << "failed to create server\n";
+            std::exit(EXIT_FAILURE);
+        }
+
+        if (enet_host_compress_with_range_coder(m_host) != 0) {
+            std::cerr << "failed to set compress with range coder\n";
+            std::exit(EXIT_FAILURE);
+        }
+    }
+
     int run(protocol& prot, std::uint32_t timeout)
     {
         prot.on_start();
